757c9181a7a032b7ba03558994c1c1f9cb741976
Mr-Another <2167503774@qq.com>
2018-10-17 12:19:38 +0800

    完善了购物车金额计算功能

--- js/cart.js
+++ js/cart.js
@@ -38,6 +38,76 @@ function like(){
     }
 }
 
+function fn1(){//全选或不选
+    $('#bottom_check').oninput=function(){
+            if($('.checkall')[1].checked==true){
+                for(let i=0;i<$('.check').length;i++){
+                    if($('.check')[i].checked==false){
+                        $('.check')[i].checked=true;
+                    }
+                }
+            }else{
+                for(let i=0;i<$('.check').length;i++){
+                    $('.check')[i].checked=false;
+            }
+        }
+        count();
+    }
+}
+
+
+
+function fn2(){//小计
+    let lis=$('#list_ul').children;   
+    let sum=0; 
+    let num=0;
+    for(let i=0;i<lis.length;i++){
+        $('.add')[i].innerHTML=$('.pro_num')[i].value*$('.unit_price')[i].innerHTML;
+        $('.subtract')[i].onclick=function(){
+            if($('.pro_num')[i].value<=0){
+                $('.pro_num')[i].value=0;
+            }else{
+                $('.pro_num')[i].value--;
+            }
+            $('.add')[i].innerHTML=$('.pro_num')[i].value*$('.unit_price')[i].innerHTML;
+            count();
+        }
+        $('.plus')[i].onclick=function(){
+            $('.pro_num')[i].value++;
+            $('.add')[i].innerHTML=$('.pro_num')[i].value*$('.unit_price')[i].innerHTML;
+            count();
+        }
+        $('.check')[i].onclick=function(){
+            if($('.check')[i].checked==true){
+                num++;
+                count();   
+            }
+        }
+        sum+=Number($('.add')[i].innerHTML);
+    }
+    $('#total_price').innerHTML=sum;
+    $('#selected').innerHTML=num;
+    $('#amount').innerHTML=lis.length;
+}
+
+function count(){//计数
+    let adds=$('.add');
+    let sum=0;
+    for(let i=0;i<adds.length;i++){
+        if($('.check')[i].checked==true){
+            sum+=Number($('.add')[i].innerHTML);
+        }
+    }
+    $('#addUp').innerHTML=sum;
+}
+
+function delect(){
+
+}
+
 window.onload=function(){
+    fn1();
+    fn2();
+    count();
     like();
 }
